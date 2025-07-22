0511b2819504f6fbf52bb7e6ef63e69840407600
chirag sankhe <chiragsankhe692001@gmail.com>
2025-07-23 04:10:07 +0530

    jobpost with all details

--- src/main/java/com/luv2code/jobportal/services/UsersService.java
+++ src/main/java/com/luv2code/jobportal/services/UsersService.java
@@ -84,4 +84,20 @@ public class UsersService {
        return null;
 
     }
+
+    public Users getCurrentUser() {
+       Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+       if(! (authentication instanceof AnonymousAuthenticationToken))
+       {
+          String username =  authentication.getName();
+          Users user = usersRepository.findByEmail(username).orElseThrow(()->
+                  new UsernameNotFoundException("could not found " + "user"));
+
+
+          return user;
+       }
+
+       return null;
+    }
 }
